3e3b5b1999edd3ff6a3d254011e233bcb2c2499c
KuRioSTmp <112512874+KuRioSTmp@users.noreply.github.com>
2023-05-22 13:49:15 +0200

    Initial commit

---
+++ Data/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using HotelBookingRESTAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingRESTAPI.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ Models/HotelBooking.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelBookingRESTAPI.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public int RoomNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Nationality { get; set; }
+    }
+}
